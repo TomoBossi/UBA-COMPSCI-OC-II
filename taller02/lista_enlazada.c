e9a0426df16708bfce86460746119c0808a05193
TomoBossi <tomasbossi97@gmail.com>
2024-04-08 18:33:02 -0300

    fix lista_enlazada.c

--- taller02/lista_enlazada.c
+++ taller02/lista_enlazada.c
@@ -14,7 +14,7 @@ lista_t* nueva_lista(void) {
 uint32_t longitud(lista_t* lista) {
     nodo_t *actual = lista->head;
     uint32_t len = 0;
-    while (actual->next != NULL) {
+    while (actual != NULL) {
         actual = actual->next;
         len += 1;
     }
@@ -23,14 +23,18 @@ uint32_t longitud(lista_t* lista) {
 
 void agregar_al_final(lista_t* lista, uint32_t* arreglo, uint64_t longitud) {
     nodo_t *actual = lista->head;
-    while (actual->next != NULL) {
-        actual = actual->next;
-    }
     nodo_t *nodo = malloc(sizeof(nodo_t)); // 24
     nodo->next = NULL;
     nodo->arreglo = arreglo;
     nodo->longitud = longitud;
-    actual->next = nodo;
+    if (actual == NULL) {
+        lista->head = nodo;
+    } else {
+        while (actual->next != NULL) {
+            actual = actual->next;
+        }
+        actual->next = nodo;
+    }
 }
 
 nodo_t* iesimo(lista_t* lista, uint32_t i) {
@@ -45,23 +49,22 @@ nodo_t* iesimo(lista_t* lista, uint32_t i) {
 
 uint64_t cantidad_total_de_elementos(lista_t* lista) {
     nodo_t *actual = lista->head;
-    uint64_t cantidad_total = actual->longitud;
-    while (actual->next != NULL) {
-        actual = actual->next;
+    uint64_t cantidad_total = 0;
+    while (actual != NULL) {
         cantidad_total += actual->longitud;
+        actual = actual->next;
     }
     return cantidad_total;
 }
 
 void imprimir_lista(lista_t* lista) {
     nodo_t *actual = lista->head;
-    uint32_t cantidad_total = 0;
-    while (actual->next != NULL) {
-        actual = actual->next;
+    while (actual != NULL) {
         uint64_t longitud = actual->longitud;
         printf("%s", "| ");
         printf("%" PRIu64, longitud);
         printf("%s", " | -> ");
+        actual = actual->next;
     }
     printf("%s", "null");
 }
@@ -80,13 +83,12 @@ int array_contiene_elemento(uint32_t* array, uint64_t size_of_array, uint32_t el
 int lista_contiene_elemento(lista_t* lista, uint32_t elemento_a_buscar) {
     nodo_t *actual = lista->head;
     int contiene = 0;
-    uint32_t cantidad_total = 0;
-    while (actual->next != NULL) {
-        actual = actual->next;
+    while (actual != NULL) {
         uint64_t longitud = actual->longitud;
         if (contiene == 0) {
             contiene = array_contiene_elemento(actual->arreglo, longitud, elemento_a_buscar);
         }
+        actual = actual->next;
     }
     return contiene;
 }
@@ -95,13 +97,13 @@ int lista_contiene_elemento(lista_t* lista, uint32_t elemento_a_buscar) {
 // Tener en cuenta que ademas, se debe liberar la memoria correspondiente a cada array de cada elemento de la lista.
 void destruir_lista(lista_t* lista) {
     nodo_t *actual = lista->head;
+    free(actual->arreglo); // En los tests, sólo el primer array es dado memoria con calloc... el resto de los arrays van al stack. ¿Por qué? Me encantaría saberlo 
+    nodo_t *tmp;
     while (actual->next != NULL) {
-        nodo_t *tmp = actual;
-        actual = actual->next;
-        free(tmp->arreglo);
-        free(tmp);
+        tmp = actual->next;
+        free(actual);
+        actual = tmp;
     }
-    free(actual->arreglo);
     free(actual);
     free(lista);
 }
